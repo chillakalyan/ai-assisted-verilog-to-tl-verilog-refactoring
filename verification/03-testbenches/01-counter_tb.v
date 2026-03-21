04a96ae90b5cac3b658cd248b05ba94e101ff49a
Chilla Kalyan <127284726+chillakalyan@users.noreply.github.com>
2026-03-21 21:38:27 +0530

    Update 01-counter_tb.v

--- verification/03-testbenches/01-counter_tb.v
+++ verification/03-testbenches/01-counter_tb.v
@@ -1,1 +1,27 @@
 
+module counter_tb;
+
+reg clk = 0;
+reg reset = 1;
+wire [3:0] count;
+
+// Instantiate DUT
+counter uut (
+    .clk(clk),
+    .reset(reset),
+    .count(count)
+);
+
+// Clock generation
+always #5 clk = ~clk;
+
+initial begin
+    $dumpfile("counter.vcd");
+    $dumpvars(0, counter_tb);
+
+    #10 reset = 0;
+
+    #100 $finish;
+end
+
+endmodule
