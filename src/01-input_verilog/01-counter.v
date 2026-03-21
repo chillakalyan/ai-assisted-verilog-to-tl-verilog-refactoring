d8c1f747d5508b8caa480c561a7ddac607667f99
Chilla Kalyan <127284726+chillakalyan@users.noreply.github.com>
2026-03-21 20:34:11 +0530

    Update 01-counter.v

--- src/01-input_verilog/01-counter.v
+++ src/01-input_verilog/01-counter.v
@@ -1,1 +1,16 @@
 
+module counter (
+    input wire clk,            
+    input wire reset,          
+    output reg [3:0] count     
+);
+
+    always @(posedge clk) begin
+        if (reset) begin
+            count <= 4'b0000;  
+        end else begin
+            count <= count + 1; 
+        end
+    end
+
+endmodule
