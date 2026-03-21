80d2341c066be281bc77396779123d4a44b3ab91
Chilla Kalyan <127284726+chillakalyan@users.noreply.github.com>
2026-03-21 21:38:42 +0530

    Update 02-alu_tb.v

--- verification/03-testbenches/02-alu_tb.v
+++ verification/03-testbenches/02-alu_tb.v
@@ -1,1 +1,20 @@
 
+module alu_tb;
+
+reg clk = 0;
+reg reset = 1;
+
+// Instantiate ALU (no outputs defined here for simplicity)
+alu_pipeline uut (
+    .clk(clk),
+    .reset(reset)
+);
+
+always #5 clk = ~clk;
+
+initial begin
+    #10 reset = 0;
+    #200 $finish;
+end
+
+endmodule
