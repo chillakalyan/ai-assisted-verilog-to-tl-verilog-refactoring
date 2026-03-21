2236ea001237a51860cc48f7da5eeac480d1fd9e
Chilla Kalyan <127284726+chillakalyan@users.noreply.github.com>
2026-03-21 21:38:56 +0530

    Update 03-fsm_tb.v

--- verification/03-testbenches/03-fsm_tb.v
+++ verification/03-testbenches/03-fsm_tb.v
@@ -1,1 +1,20 @@
 
+module fsm_tb;
+
+reg clk = 0;
+reg reset = 1;
+
+// Instantiate FSM
+fsm_traffic_light uut (
+    .clk(clk),
+    .reset(reset)
+);
+
+always #5 clk = ~clk;
+
+initial begin
+    #10 reset = 0;
+    #200 $finish;
+end
+
+endmodule
